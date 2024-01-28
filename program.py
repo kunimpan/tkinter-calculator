dd10d3a00631136822d2170b759303f2d0310aea
Pun <impan_1611_2544@hotmail.com>
2024-01-28 17:17:22 +0700

    Add:gridLayout btn

--- program.py
+++ program.py
@@ -35,7 +35,7 @@ btnSquare=Button(buttonFrame, text="x^2", font=btnFont, bg=color)
 btnExponent=Button(buttonFrame, text="x^n", font=btnFont, bg=color)
 btnDivide=Button(buttonFrame, text="/", font=btnFont, bg=color)
 btnMultiply=Button(buttonFrame, text="x", font=btnFont, bg=color)
-btnSubtract=Button(buttonFrame, text="1/x", font=btnFont, bg=color)
+btnSubtract=Button(buttonFrame, text="-", font=btnFont, bg=color)
 btnAdd=Button(buttonFrame, text="+", font=btnFont, bg=color)
 btnEqual=Button(buttonFrame, text="=", font=btnFont, bg=color)
 btnDecimal=Button(buttonFrame, text=".", font=btnFont, bg=color)
@@ -53,6 +53,34 @@ btn2=Button(buttonFrame, text="2", font=btnFont, bg="black", fg="white")
 btn1=Button(buttonFrame, text="1", font=btnFont, bg="black", fg="white")
 btn0=Button(buttonFrame, text="0", font=btnFont, bg="black", fg="white")
 
+#row1
+btnInverse.grid(row=1, column=0, pady=1, ipadx=10, sticky="WE")
+btnSquare.grid(row=1, column=1, pady=1, ipadx=10, sticky="WE")
+btnExponent.grid(row=1, column=2, pady=1, ipadx=10, sticky="WE")
+btnDivide.grid(row=1, column=3, pady=1, ipadx=10, sticky="WE")
 
+#row2
+btn7.grid(row=2, column=0, pady=1, ipadx=10, sticky="WE")
+btn8.grid(row=2, column=1, pady=1, ipadx=10, sticky="WE")
+btn9.grid(row=2, column=2, pady=1, ipadx=10, sticky="WE")
+btnMultiply.grid(row=2, column=3, pady=1, ipadx=10, sticky="WE")
+
+#row3
+btn4.grid(row=3, column=0, pady=1, ipadx=10, sticky="WE")
+btn5.grid(row=3, column=1, pady=1, ipadx=10, sticky="WE")
+btn6.grid(row=3, column=2, pady=1, ipadx=10, sticky="WE")
+btnSubtract.grid(row=3, column=3, pady=1, ipadx=10, sticky="WE")
+
+#row4
+btn1.grid(row=4, column=0, pady=1, ipadx=10, sticky="WE")
+btn2.grid(row=4, column=1, pady=1, ipadx=10, sticky="WE")
+btn3.grid(row=4, column=2, pady=1, ipadx=10, sticky="WE")
+btnAdd.grid(row=4, column=3, pady=1, ipadx=10, sticky="WE")
+
+#row5
+btnNegate.grid(row=5, column=0, pady=1, ipadx=10, sticky="WE")
+btn0.grid(row=5, column=1, pady=1, ipadx=10, sticky="WE")
+btnDecimal.grid(row=5, column=2, pady=1, ipadx=10, sticky="WE")
+btnEqual.grid(row=5, column=3, pady=1, ipadx=10, sticky="WE")
 
 root.mainloop()
