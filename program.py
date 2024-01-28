97da5eb8be09d12da16b871964287a3a0d3d501e
Pun <impan_1611_2544@hotmail.com>
2024-01-28 17:00:13 +0700

    Add:opeator button

--- program.py
+++ program.py
@@ -26,7 +26,20 @@ display.pack(padx=5, pady=5)
 #clear & quit
 btnClear=Button(buttonFrame, text="Clear", font=btnFont)
 btnQuit=Button(buttonFrame, text="Quit", font=btnFont, command=root.destroy)
-btnClear.grid(row=0, column=0, columnspan=2, ipadx=30, sticky="WE") # columnspan is taking up space.
-btnQuit.grid(row=0, column=2, columnspan=2, ipadx=30, sticky="WE")
+btnClear.grid(row=0, column=0, columnspan=2, ipadx=35, sticky="WE") # columnspan is taking up space.
+btnQuit.grid(row=0, column=2, columnspan=2, ipadx=35, sticky="WE")
+
+#opeator button
+btnInverse=Button(buttonFrame, text="1/x", font=btnFont, bg=color)
+btnSquare=Button(buttonFrame, text="x^2", font=btnFont, bg=color)
+btnExponent=Button(buttonFrame, text="x^n", font=btnFont, bg=color)
+btnDivide=Button(buttonFrame, text="/", font=btnFont, bg=color)
+btnMultiply=Button(buttonFrame, text="x", font=btnFont, bg=color)
+btnSubtract=Button(buttonFrame, text="1/x", font=btnFont, bg=color)
+btnAdd=Button(buttonFrame, text="+", font=btnFont, bg=color)
+btnEqual=Button(buttonFrame, text="=", font=btnFont, bg=color)
+btnDecimal=Button(buttonFrame, text=".", font=btnFont, bg=color)
+btnNegate=Button(buttonFrame, text="+/-", font=btnFont, bg=color)
+
 
 root.mainloop()
