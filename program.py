f5fe038c1dde581b62a772e140f93384ce50ce03
Pun <impan_1611_2544@hotmail.com>
2024-01-28 14:51:52 +0700

    Create:LabelFrame

--- program.py
+++ program.py
@@ -1,10 +1,23 @@
 from tkinter import *
 
-
+# design root windows
 root=Tk()
 root.title("Calculator")
 root.iconbitmap("icon/cal-logo.ico")
 root.geometry("300x400")
 root.resizable(0, 0)
+
+#settings
+color="orange"
+displayFont=("Arail", 35)
+btnFont=("Arail", 19)
+
+
+#design frame
+displayFrame=LabelFrame(root)
+buttonFrame=LabelFrame(root)
+
+displayFrame.pack(padx=2, pady=5)
+buttonFrame.pack(pady=2)
 
 root.mainloop()
